67a343655c73fddc1e51d0bcec7a31bd9ba0af4f
Mathys INFOSI <56838862+Wygoo@users.noreply.github.com>
2025-12-08 16:24:04 +0100

    Squelette Pièces

---
+++ JeuDame/src/main/java/org/centrale/jeudame/Action.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.centrale.jeudame;
+
+/**
+ *
+ * @author dytri
+ */
+public interface Action {
+    public void Deplacer(Plateau plateau);
+    public void Prendre(Plateau plateau);
+}
---
+++ JeuDame/src/main/java/org/centrale/jeudame/Pion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.jeudame;
+
+/**
+ *
+ * @author dytri
+ */
+public class Pion extends Piece implements Action{
+    
+    public void Deplacer(Plateau plateau){
+        
+    }
+    
+    public void Prendre(Plateau plateau){
+        
+    }
+}
