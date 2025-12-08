4d3b5890b28dcda47837a4ad98141fcc55ca7429
romainP44 <romain.perrier@eleves.ec-nantes.fr>
2025-12-08 17:01:47 +0100

    Update Plateau.java

--- JeuDame/src/main/java/org/centrale/jeudame/Plateau.java
+++ JeuDame/src/main/java/org/centrale/jeudame/Plateau.java
@@ -9,5 +9,40 @@ package org.centrale.jeudame;
  * @author rompe
  */
 public class Plateau {
-    
+    private static final int SIZE = 8;
+
+    /**
+     * Génère un plateau de dames 8x8.
+     * Les cases sombres et claires alternent.
+     */
+    public static String[][] generateBoard() {
+        String[][] board = new String[SIZE][SIZE];
+
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                // Alternance des couleurs
+                boolean dark = (row + col) % 2 != 0;
+
+                // Symboles Unicode pour affichage console
+                board[row][col] = dark ? "⬛" : "⬜";
+            }
+        }
+
+        return board;
+    }
+
+    /**
+     * Affiche le plateau en console.
+     */
+    public static void printBoard(String[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            StringBuilder line = new StringBuilder();
+            for (int col = 0; col < SIZE; col++) {
+                line.append(board[row][col]);
+            }
+            System.out.println(line);
+        }
+    }
+
+
 }
